b232103c119b4b42337c6521f479779604eed401
joryhe <1032473410@qq.com>
2016-05-15 14:38:53 +0800

    Site updated: 2016-05-15 14:38:51

--- vendors/jquery_lazyload/jorylazyloadstart.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-      jQuery(function() {  
-          jQuery("img.jory_lazyimg").lazyload({
-			  delay:5000
-              effect:"fadeIn"
-            });
-          });
-</script> 
